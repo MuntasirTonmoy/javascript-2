371249230cd61777b8482b22069c6907d1ec0763
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-28 17:20:09 +0600

    lecture 2 array declare and array indexing

---
+++ lecture-2-js-array.js
@@ -0,0 +1,26 @@
+//js array
+// use better comment extension
+// array means a variable in which we can store more than one value
+// *we can write array by putting the value inside two box bracket []    
+
+// declaring array
+var myFriendsAge = [23, 24, 25];
+var muriChanachurFee = [5, 10, 50, 100, 3];
+
+// * we can store string in array also. ex:
+var myFamily = ['me & pory', 'mim', 'modhu', 'vaiya', 'abba amma'];
+
+// other array example
+var oddNumbers = [1, 3, 5, 7, 9];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+
+//  we can see how many element inside array by using
+//* variabeName.length
+console.log(vowels.length);
+
+// array value position starts from zero
+// * it is also called array index means index starts from zero
+
+
