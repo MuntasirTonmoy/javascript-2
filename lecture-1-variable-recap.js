f31e802e4dce8a3beed34dca76e10a18ec4146e9
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-28 16:18:54 +0600

    javascript-2 lecture-1-variable recap

---
+++ lecture-1-variable-recap.js
@@ -0,0 +1,17 @@
+//Numeric
+// var indicates declaring a variable
+var watchPrice = 150;
+watchPrice = 160; // value of variable can vary
+
+//string
+var name = 'Muntasir Tonmoy';
+
+// boolean
+var isExpensive = ture;
+
+// we can add two string
+// we can do math operation addition subtraction multiplication division and modulu
+// we can use short hand like += -=
+// if we have to increase or decrease only 1 then we can use increament or decrement operator ++ or --
+// we can use parseInt() or parseFloat() to get only integer and float value
+// we can use variableName.toFixed(value of how many digit we want to see after decimal point) with parseFloat(); function to see specific number of digit after decimal point.
