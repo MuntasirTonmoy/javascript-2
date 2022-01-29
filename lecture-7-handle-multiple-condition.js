84ee1a622f6586a88ef40f468601f5c44236accc
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-29 18:49:51 +0600

    l7 multiple condition

---
+++ lecture-7-handle-multiple-condition.js
@@ -0,0 +1,10 @@
+var getJob = true;
+var savedMoney = 200000;
+//*  use tabout extension to get out from the bracket
+if (getJob == true && savedMoney > 100000) //we use && if both of the condition is true.
+{
+    console.log('marriage done');
+}
+else {
+    console.log('marrige not don');
+}
