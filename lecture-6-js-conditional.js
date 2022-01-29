f6054fad4eee39b2aa4c0158a75d6662cb4cc03b
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-29 17:13:35 +0600

    lecture-6 conditional

---
+++ lecture-6-js-conditional.js
@@ -0,0 +1,19 @@
+var isFoodReady = true;
+// if food is ready then I will eat
+if (isFoodReady == true) {
+    console.log('I will eat now');
+}
+else {
+    console.log('I will watch video.');
+}
+
+// buying Iphone
+
+var iphonePrice = 70000;
+var myBudget = 5000;
+if (iphonePrice < myBudget) {
+    console.log('I will buy an Iphone.')
+}
+else {
+    console.log('My samsung is the best');
+}
