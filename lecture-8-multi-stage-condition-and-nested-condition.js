68fda5b29d0e6b22cb38ca6be90aa84a8fd9f6e2
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-01-30 20:24:25 +0600

    else if and nested if else

---
+++ lecture-8-multi-stage-condition-and-nested-condition.js
@@ -0,0 +1,43 @@
+//* multi stage coditional
+var cgpa = [4, 3.5, 3, 2.5, 2, 0];
+var marks = 39;
+
+if (marks <= 100 && marks >= 80) {
+    console.log('Your cgpa is ' + cgpa[0]);
+}
+else if (marks <= 79 && marks >= 70) {
+    console.log('Your cgpa is ' + cgpa[1]);
+}
+else if (marks <= 69 && marks >= 60) {
+    console.log('Your cgpa is ' + cgpa[2]);
+}
+else if (marks <= 59 && marks >= 50) {
+    console.log('Your cgpa is ' + cgpa[3]);
+}
+else if (marks <= 49 && marks >= 40) {
+    console.log('Your cgpa is ' + cgpa[4]);
+}
+else {
+    console.log('Your cgpa is ' + cgpa[5]);
+}
+
+//* nested conditional
+// Leap year in nested 
+var year = 2024;
+
+if (year % 4 == 0) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            console.log('Leap Year.');
+        }
+        else {
+            console.log('Not leap year');
+        }
+    }
+    else {
+        console.log('Leap year');
+    }
+}
+else {
+    console.log('Not leap year');
+}
